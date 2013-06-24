1c75afbb6a662a18cf98be4596009e9f6b1bf59a
Henry Wang <henry.t.wang@gmail.com>
2013-06-24 18:52:13 -0500

    add links and gravatar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Personal::Application.routes.draw do
+
+  resources :home
+
+  root :to => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
